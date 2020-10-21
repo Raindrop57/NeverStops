23cf88018c1166cc9741bee4f8e25a5b5d703e5b
Raindrop57 <julian.c.schima-1@ou.edu>
2020-10-21 16:19:44 -0500

    Add stuff to .gitignore

--- src/NeverStops.java
+++ src/NeverStops.java
@@ -1,8 +1,11 @@
 
 public class NeverStops {
 	public static void main(String[] args) {
-		while (true) {
+		int i = 0;
+		while (i < 1000) {
 			System.out.println("This code never stops");
+			++i;
 		}
+		System.out.println("Or does it?");
 	}
 }
